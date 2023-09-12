c28b41b73abe0145456d121d467372efb4661ddc
Ot225 <othmanalani225@gmail.com>
2023-09-12 14:32:31 +0200

    JS server

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log("Hello World")
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
 
+app.get('/', (req,res) => {
+
+        res.send('<h1> Press Here! </h1>');
+
+});
+
+server.listen(4040, () => {
+	console.log("Server on 4040 port");
+});
